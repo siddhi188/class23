14bb569df4590d460b9bba4fea88f7169d325fbc
siddhi188 <73110622+siddhi188@users.noreply.github.com>
2020-12-08 16:48:50 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,24 @@
+class Box{
+    constructor(x,y,width,height){
+    var options={
+    restitution :1 
+
+    }
+    this.body = Bodies.rectangle(x,y,width,height,options)
+    this.x=x
+    this.y=y
+    this.width=width
+    this.height=height
+    World.add(world,this.body)
+    }
+    display (){
+    push ();
+    translate(this.body.position.x,this.body.position.y)
+    rotate (this.body.angle)
+    rectMode(CENTER)
+    rect(0,0,this.width,this.height);
+    pop ();
+    }
+
+
+}
